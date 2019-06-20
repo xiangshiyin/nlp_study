712e5779383a4c8248bc4593dff3daad8328bfb1
Xiangshi Yin <piupiuyin@gmail.com>
2019-06-20 11:16:34 -0400

    miscellaneous

--- src/word2vec_test.py
+++ src/word2vec_test.py
@@ -1,6 +1,24 @@
+import os
+import urllib
+
+def maybe_download(filename, url, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
 
 def main():
-    pass
+    # pass
+    url = 'http://mattmahoney.net/dc/'
+    filename = maybe_download('text8.zip', url, 31344016)
+    print(filename)
 
 if __name__ == "__main__":
     main()
